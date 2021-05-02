83b8c33d831e00814ee7f6cbf26185f2b3520fbf
PRF <462316190@qq.com>
2021-05-03 02:14:52 +0800

    有效括弧算法题提交
    有效括弧算法题提交

--- LeeCode/LeeCode/ViewController.swift
+++ LeeCode/LeeCode/ViewController.swift
@@ -13,23 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-//        let nums = [-82,-70,-66,-49,-43,-29,-29,-14,-11,-6,-3,-3,1,2,10,12,13,15,15,17,21,26,26,28,28,29,31,33,34,36,43,46,46,47,48,49,52,55,55,56,57,61,62,65,69,71,74,76,77,79,83,84,86,93,94]
+        let bbb = ValidBrackets.isValid2("{[]}")
         
-//        let nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
-        
-        let nums = [0,0,0]
-        let sortNums = nums.sorted()
-
-        print("sortNums:\(sortNums)")
-
-        let result = ThreeSum.threeSum(sortNums)
-
-        print("result\(result)")
-
-        print("得到结果了")
-        
-        
-      
+        if bbb {
+            print("成对")
+        }else{
+            print("不成对")
+        }
     }
 }
 
---
+++ LeeCode/LeeCode/有效的括号/ValidBrackets.swift
@@ -0,0 +1,124 @@
+//
+//  Avalible.swift
+//  LeeCode
+//
+//  Created by 庞日富 on 2021/5/2.
+//
+
+import Foundation
+
+class ValidBrackets {
+    
+    //方案1
+    static func isValid0(_ str:String) -> Bool{
+        
+        //1判断如果字符串长度为单数,不可能有效,因为括号必须成对出现
+        if str.count % 2 != 0 || str.count < 2 {
+            return false
+        }
+        
+        //2.第一个字符不能是右边的括号类型:[")","]","}"]
+        let first = str.first!
+        if [")","]","}"].contains("\(first)") {
+            return false
+        }
+        
+        let leftBrackets = ["(","[","{"]
+        var leftBracketsValid = [String]()
+        let keyValue = [")":"(","]":"[","}":"{"]
+        
+        for i in 0...str.count-1 {
+            let charecter = "\(str[str.index(str.startIndex, offsetBy: i)])"
+            
+            if leftBrackets.contains(charecter) {
+                leftBracketsValid.append(charecter)
+            }else{
+                let lastBracket = leftBracketsValid.last ?? ""
+                
+                if keyValue[charecter] == lastBracket {
+                    leftBracketsValid.removeLast()
+                }else{
+                    return false
+                }
+            }
+        }
+        
+        if leftBracketsValid.count > 0 {
+            return false
+        }else {
+            return true
+        }
+    }
+    
+    
+    //方案2
+    static func isValid(_ s: String) -> Bool {
+        //声明一个栈
+        var stack = [Character]();
+        //遍历字符
+        for c:Character in s {
+            
+            if (c == "{") || (c == "(") || (c == "[") {
+                stack.append(c);
+            } else {
+                if stack.isEmpty {
+                    return false;
+                }
+                let left:Character = stack.removeLast();
+                
+                if left == "{" && c != "}" {
+                    return false;
+                }
+                if left == "[" && c != "]" {
+                    return false;
+                }
+                if left == "(" && c != ")" {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty;
+        }
+    
+    
+    static func isValid2(_ str:String) -> Bool{
+        
+        //1判断如果字符串长度为单数,不可能有效,因为括号必须成对出现
+        if str.count % 2 != 0 || str.count < 2 {
+            return false
+        }
+        
+        //2.第一个字符不能是右边的括号类型:[")","]","}"]
+        let first = str.first!
+        
+        if (first==")") || (first == "]") || (first == "}") {
+            return false
+        }
+        
+        var stack = [Character]()
+        let keyMap:[Character:Character] = [")":"(","]":"[","}":"{"]
+        
+        for c:Character in str {
+            
+            if (c == "{") || (c == "(") || (c == "[") {
+                stack.append(c);
+            }else{
+                if stack.isEmpty {
+                    return false
+                }else{
+                    
+                    let left:Character = stack.removeLast();
+                    
+                    if keyMap[c] != left {
+                        return false
+                    }
+                }
+            }
+        }
+        if stack.count > 0 {
+            return false
+        }else {
+            return true
+        }
+    }
+}
